a5a1c36859ac7616bc6b7929cdd9afc99f559340
swcollard <swcollard12@gmail.com>
2017-12-23 12:24:44 -0600

    Use environment variables

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,14 +1,15 @@
 from __future__ import print_function
 from twitter import Twitter, OAuth
+import os
 
 """
  Twitter Vars
 """
 request_url = 'https://api.twitter.com/1.1/trends/place.json?id=1'
-access_key = '717077395-OZhKTbYQPGdD2N5ZrKxsKcQN7spofhmschwLUbk1'
-access_secret = 'j0rqs8UB9s7j5wI0Trs5aeKmUoQ80z9dxc8CdwQnVO3uK'
-consumer_key = '5DXDPPphnJDplxrUNhIgyMSrZ'
-consumer_secret = 'XhE0jiiU3cOZs7v44BQTCbGPvB3OpcT6VLzwn0SpIEqlBAHWfh'
+access_key = os.environ['access_key']
+access_secret = os.environ['access_secret']
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
 twitter = Twitter(auth = OAuth(access_key, access_secret, consumer_key, consumer_secret))
 
 # ------------ Fetch Twitter Trends ---------------
